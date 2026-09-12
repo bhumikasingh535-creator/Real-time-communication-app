cf5b874c63362adec16967ff7feca35cafe4a6b2
Bhumika Singh <bhumikasingh535@gmail.com>
2026-09-12 19:50:14 +0530

    Use production API environment variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "https://real-time-communication-app-1-gwmu.onrender.com/api",
+ baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use(
